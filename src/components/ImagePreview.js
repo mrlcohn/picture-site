9ce98db562a51376da06fb7556af5a597c82c56a
Leonard Cohn <leonardacohn@gmail.com>
2023-09-04 11:17:33 -0500

    Successful data fetching through backend API

--- src/components/ImagePreview.js
+++ src/components/ImagePreview.js
@@ -2,16 +2,12 @@ import useFetch from '../hooks/useFetch';
 import '../index.css';
 
 const ImagePreview = ({ title, path }) => {
-  const { data, isPending } = useFetch('https://server.cohn-family.photos/photos' + path);
-
-  const src = data;
-  
-  console.log(src);
+  const { data: src, isPending } = useFetch('https://server.cohn-family.photos/photos/' + path);
 
   return ( 
     <div>
       { isPending && <div>Loading...</div> }
-      { data &&
+      { src &&
       <div className="image-preview">
         <h2>{ title }</h2>
         <img src={src.url} alt="Error loading image" />
